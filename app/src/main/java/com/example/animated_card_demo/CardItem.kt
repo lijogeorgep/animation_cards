b78085935478f87fa57492f27f061843805fe1fc
Lijo George <lachulijo3@gmail.com>
2024-02-13 11:55:37 +0530

    Initial commit

---
+++ app/src/main/java/com/example/animated_card_demo/CardItem.kt
@@ -0,0 +1,3 @@
+package com.example.animated_card_demo
+
+data class CardItem(val title: String, val description: String)
---
+++ app/src/main/java/com/example/animated_card_demo/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.animated_card_demo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val cardList = ArrayList<CardItem>()
+        for (i in 1..5) {
+            cardList.add(CardItem("Title $i", "Description $i"))
+        }
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = CardAdapter(cardList)
+    }
+}
